a1ed9eb139c2fc1ad08a1deb5ef25772fefe5512
Luiz <luis231103@gmail.com>
2023-04-24 08:48:26 -0300

    feat: criando rotas,controllers e views

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class SupportController extends Controller
+{
+    public function index() {
+        return view('admin.supports.index');
+    }
+}
