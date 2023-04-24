9ca0bb33721cb0d1eafa2a7bcc195d6e22aa4a36
Luiz <luis231103@gmail.com>
2023-04-24 09:21:55 -0300

    feat: listagem com blade

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,3 +1,21 @@
 <h1>Listagem dos suportes</h1>
 
-<a href="{{ route('supports.create') }}">Criar dúvida</a>
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descricao</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support )
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->body }}</td>
+                <td>-</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
