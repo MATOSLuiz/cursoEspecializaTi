f36e39847c57c3b709b30acb8620d6338c8e5a91
Luiz <luis231103@gmail.com>
2023-04-24 11:03:49 -0300

    feat: método delete de supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -57,4 +57,14 @@ class SupportController extends Controller
 
         return redirect()->route('supports.index');
     }
+
+    public function destroy(string|int $id, Support $support) {
+        if (!$support = $support->find($id)) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
 }
